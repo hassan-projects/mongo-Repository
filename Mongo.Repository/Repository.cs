4d3508dc77437492da302e2fa11e7b39075b5d0e
Hassan Bazuhair <hassanhgfdplay@gmail.com>
2020-09-16 11:22:14 +0300

    add Repository class

---
+++ Mongo.Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Repository.Mongo;
+
+
+namespace Mongo.Repository
+{
+    public class Repository<T> : global::Repository.Mongo.Repository<T> where T : IEntity
+    {
+        #region constructors
+
+        public Repository(IMongoDatabase mongoDatabase) : base(mongoDatabase)
+        {
+        }
+
+        public Repository(IMongoDatabase mongoDatabase, string collectionName) : base(mongoDatabase, collectionName)
+        {
+        }
+
+        public Repository(IMongoClient mongoClient, string databaseName) : base(mongoClient, databaseName)
+        {
+        }
+
+        public Repository(IMongoClient mongoClient, string databaseName, string collectionName) : base(mongoClient, databaseName, collectionName)
+        {
+        }
+
+        public Repository(IConfiguration config) : base(config)
+        {
+        }
+
+        public Repository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public Repository(string connectionString, string collectionName) : base(connectionString, collectionName)
+        {
+        }
+
+        #endregion
+
+    }
+}
